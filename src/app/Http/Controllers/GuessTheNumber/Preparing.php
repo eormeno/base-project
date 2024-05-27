da6b6018d93d66148ed6c031131a39f6732270f9
Emilio Ormeño <eormneno@gmail.com>
2024-05-27 15:52:50 -0300

    minimal change

--- src/app/Http/Controllers/GuessTheNumber/Preparing.php
+++ src/app/Http/Controllers/GuessTheNumber/Preparing.php
@@ -6,10 +6,14 @@ use App\FSM\StateAbstractImpl;
 
 class Preparing extends StateAbstractImpl
 {
-    public function handleRequest(?string $event = null, $data = null)
+    public function onEnter(): void
     {
         $this->context->random_number = rand(Globals::MIN_NUMBER, Globals::MAX_NUMBER);
         $this->context->remaining_attempts = Globals::maxAttempts();
+    }
+
+    public function handleRequest(?string $event = null, $data = null)
+    {
         $this->context->setState(Playing::class);
     }
 }
