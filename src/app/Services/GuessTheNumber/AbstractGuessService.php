1a79f1565f1089a7056f9ca2a843fd5ac96cf0b0
Emilio Ormeño <eormneno@gmail.com>
2024-06-01 16:22:34 -0300

    Make abstract service for encapsulation

---
+++ src/app/Services/GuessTheNumber/AbstractGuessService.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Services\GuessTheNumber;
+
+use App\Exceptions\GuessTheNumber\FailException;
+use App\Exceptions\GuessTheNumber\InfoException;
+use App\Exceptions\GuessTheNumber\SuccessException;
+use App\Repositories\GuessTheNumber\GameRepository;
+use App\Exceptions\GuessTheNumber\GameOverException;
+use App\Exceptions\GuessTheNumber\NotInRangeException;
+
+abstract class AbstractGuessService
+{
+    public function __construct(
+        protected GameRepository $gameRepository,
+        protected GameConfigService $gameConfigService,
+        protected MessageService $messageService,
+    ) {
+    }
+
+    public function updateRemainingAttempts(int $remainingAttempts): void
+    {
+        $game = $this->gameRepository->getGame();
+        $game->remaining_attempts = $remainingAttempts;
+        $game->save();
+    }
+
+    public function decreaseRemainingAttempts(): void
+    {
+        $game = $this->gameRepository->getGame();
+        $game->remaining_attempts--;
+        $game->save();
+    }
+
+    protected function checkNumberIsCheat($number)
+    {
+        if ($number == $this->gameConfigService->getCheatNumber()) {
+            $this->updateRemainingAttempts(1);
+            throw new InfoException($this->messageService->cheatMessage());
+        }
+    }
+
+    protected function checkNumberIsNotBetween($number)
+    {
+        $min = $this->gameConfigService->getMinNumber();
+        $max = $this->gameConfigService->getMaxNumber();
+        if ($number < $min || $number > $max) {
+            $message = $this->messageService->invalidNumberMessage();
+            throw new NotInRangeException($message, $number, $min, $max);
+        }
+    }
+
+    protected function checkNoEnoughAttempts()
+    {
+        $game = $this->gameRepository->getGame();
+        if ($game->remaining_attempts <= 1) {
+            throw new GameOverException();
+        }
+    }
+
+    protected function checkNumberIsLowerThanRandomNumber($number)
+    {
+        $game = $this->gameRepository->getGame();
+
+        if ($number < $game->random_number) {
+            $this->decreaseRemainingAttempts();
+            throw new FailException($this->messageService->greaterMessage($number));
+        }
+    }
+
+    protected function checkNumberIsGreaterThanRandomNumber($number)
+    {
+        $game = $this->gameRepository->getGame();
+        if ($number > $game->random_number) {
+            $this->decreaseRemainingAttempts();
+            throw new FailException($this->messageService->lowerMessage($number));
+        }
+    }
+
+    protected function checkNumberIsGuessed($number)
+    {
+        $game = $this->gameRepository->getGame();
+        if ($number == $game->random_number) {
+            throw new SuccessException();
+        }
+    }
+}
--- src/app/Services/GuessTheNumber/GuessService.php
+++ src/app/Services/GuessTheNumber/GuessService.php
@@ -2,22 +2,8 @@
 
 namespace App\Services\GuessTheNumber;
 
-use App\Exceptions\GuessTheNumber\FailException;
-use App\Exceptions\GuessTheNumber\InfoException;
-use App\Exceptions\GuessTheNumber\SuccessException;
-use App\Repositories\GuessTheNumber\GameRepository;
-use App\Exceptions\GuessTheNumber\GameOverException;
-use App\Exceptions\GuessTheNumber\NotInRangeException;
-
-class GuessService
+class GuessService extends AbstractGuessService
 {
-    public function __construct(
-        protected GameRepository $gameRepository,
-        protected GameConfigService $gameConfigService,
-        protected MessageService $messageService,
-    ) {
-    }
-
     public function guess($number)
     {
         $this->checkNumberIsCheat($number);
@@ -28,70 +14,4 @@ class GuessService
         $this->checkNumberIsGreaterThanRandomNumber($number);
     }
 
-    public function updateRemainingAttempts(int $remainingAttempts): void
-    {
-        $game = $this->gameRepository->getGame();
-        $game->remaining_attempts = $remainingAttempts;
-        $game->save();
-    }
-
-    public function decreaseRemainingAttempts(): void
-    {
-        $game = $this->gameRepository->getGame();
-        $game->remaining_attempts--;
-        $game->save();
-    }
-
-    private function checkNumberIsCheat($number)
-    {
-        if ($number == $this->gameConfigService->getCheatNumber()) {
-            $this->updateRemainingAttempts(1);
-            throw new InfoException($this->messageService->cheatMessage());
-        }
-    }
-
-    private function checkNumberIsNotBetween($number)
-    {
-        $min = $this->gameConfigService->getMinNumber();
-        $max = $this->gameConfigService->getMaxNumber();
-        if ($number < $min || $number > $max) {
-            $message = $this->messageService->invalidNumberMessage();
-            throw new NotInRangeException($message, $number, $min, $max);
-        }
-    }
-
-    private function checkNoEnoughAttempts()
-    {
-        $game = $this->gameRepository->getGame();
-        if ($game->remaining_attempts <= 1) {
-            throw new GameOverException();
-        }
-    }
-
-    private function checkNumberIsLowerThanRandomNumber($number)
-    {
-        $game = $this->gameRepository->getGame();
-
-        if ($number < $game->random_number) {
-            $this->decreaseRemainingAttempts();
-            throw new FailException($this->messageService->greaterMessage($number));
-        }
-    }
-
-    private function checkNumberIsGreaterThanRandomNumber($number)
-    {
-        $game = $this->gameRepository->getGame();
-        if ($number > $game->random_number) {
-            $this->decreaseRemainingAttempts();
-            throw new FailException($this->messageService->lowerMessage($number));
-        }
-    }
-
-    private function checkNumberIsGuessed($number)
-    {
-        $game = $this->gameRepository->getGame();
-        if ($number == $game->random_number) {
-            throw new SuccessException();
-        }
-    }
 }
