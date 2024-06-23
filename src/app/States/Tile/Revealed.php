8868737b281009b91e9e9fa055a74282a18c9058
Emilio Ormeño <eormneno@gmail.com>
2024-06-23 11:55:35 -0300

    minimal changes

--- src/app/States/Tile/Revealed.php
+++ src/app/States/Tile/Revealed.php
@@ -8,6 +8,7 @@ class Revealed extends StateAbstractImpl
 {
     public bool $hasTrap = false;
     public int $trapsAround = 0;
+
     public function onRefresh(): void
     {
         $this->hasTrap = $this->model->hasTrap;
--- src/resources/views/mythic-treasure-quest/revealed.blade.php
+++ src/resources/views/mythic-treasure-quest/revealed.blade.php
@@ -1,5 +1,5 @@
-<div class="w-10 h-10 bg-slate-700 text-gray-400 text-xs hover:bg-slate-500 flex items-center justify-center">
-    <p>
+<div class="w-10 h-10 bg-slate-700 text-gray-400 text-xs hover:bg-slate-600 flex items-center justify-center">
+    <div>
         @if ($hasTrap)
         <div class="text-red-500">
             <x-trap />
@@ -8,5 +8,5 @@
         @if (!$hasTrap && $trapsAround > 0)
             {{ $trapsAround }}
         @endif
-    </p>
+    </div>
 </div>
