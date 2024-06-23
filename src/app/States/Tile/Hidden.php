9c6385d2a6bdebbf69e9951adb06d0a5bae6f6e3
Emilio Ormeño <eormneno@gmail.com>
2024-06-23 11:47:38 -0300

    minimal changes. removed event and button from revealed state view

--- src/app/States/Tile/Hidden.php
+++ src/app/States/Tile/Hidden.php
@@ -8,11 +8,13 @@ class Hidden extends StateAbstractImpl
 {
     public bool $hasTrap = false;
     public int $trapsAround = 0;
+
     public function onRefresh(): void
     {
         $this->hasTrap = $this->model->hasTrap;
         $this->trapsAround = $this->model->trapsAround;
     }
+
     public function onTileOffClickEvent()
     {
         return Revealed::StateClass();
--- src/resources/views/mythic-treasure-quest/revealed.blade.php
+++ src/resources/views/mythic-treasure-quest/revealed.blade.php
@@ -1,9 +1,10 @@
-<button onclick="sendEvent('tile_on_click')">
-    <div class="w-10 h-10 bg-slate-700 text-gray-300 text-xs hover:bg-slate-500 flex items-center justify-center">
-        <p>
-            @if (!$hasTrap && $trapsAround > 0)
-                {{ $trapsAround }}
-            @endif
-        </p>
-    </div>
-</button>
+<div class="w-10 h-10 bg-slate-700 text-gray-300 text-xs hover:bg-slate-500 flex items-center justify-center">
+    <p>
+        @if ($hasTrap)
+            X
+        @endif
+        @if (!$hasTrap && $trapsAround > 0)
+            {{ $trapsAround }}
+        @endif
+    </p>
+</div>
