04b2c725a63f20cb746fb1bd2790ba9123392e1e
Emilio Ormeño <eormneno@gmail.com>
2024-06-23 11:53:08 -0300

    added trap component

--- src/resources/views/mythic-treasure-quest/revealed.blade.php
+++ src/resources/views/mythic-treasure-quest/revealed.blade.php
@@ -1,7 +1,9 @@
-<div class="w-10 h-10 bg-slate-700 text-gray-300 text-xs hover:bg-slate-500 flex items-center justify-center">
+<div class="w-10 h-10 bg-slate-700 text-gray-400 text-xs hover:bg-slate-500 flex items-center justify-center">
     <p>
         @if ($hasTrap)
-            X
+        <div class="text-red-500">
+            <x-trap />
+        </div>
         @endif
         @if (!$hasTrap && $trapsAround > 0)
             {{ $trapsAround }}
