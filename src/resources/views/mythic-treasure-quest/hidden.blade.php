4ab6af636305af5f3d341f5ff8a548ebb1ff7e36
DAMO\Emilio <eormeno@gmail.com>
2024-06-27 08:37:28 -0300

    visual improvements

--- src/resources/views/mythic-treasure-quest/hidden.blade.php
+++ src/resources/views/mythic-treasure-quest/hidden.blade.php
@@ -1,5 +1,5 @@
 <button onclick="sendEvent('tile_off_click')"">
-    <div class="w-10 h-10 bg-slate-800 text-gray-500 text-xs hover:bg-slate-700 flex items-center justify-center border rounded-md border-slate-700">
+    <div class="w-10 h-10 bg-slate-800 text-gray-500 text-xs hover:bg-slate-700 flex items-center justify-center border rounded-md border-slate-600">
         <p></p>
     </div>
 </button>
--- src/resources/views/mythic-treasure-quest/revealed.blade.php
+++ src/resources/views/mythic-treasure-quest/revealed.blade.php
@@ -1,4 +1,4 @@
-<div class="w-10 h-10 bg-slate-700 text-gray-400 text-xs hover:bg-slate-600 flex items-center justify-center">
+<div class="w-10 h-10 bg-slate-600 text-gray-400 text-xs hover:bg-slate-500 flex items-center justify-center">
     <div>
         @if ($hasTrap)
         <div class="text-red-500">
