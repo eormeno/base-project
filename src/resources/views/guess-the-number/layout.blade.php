cd3ce3f9548deb5857cc9d07f09adbbfa512f994
DAMO\Emilio <eormeno@gmail.com>
2024-12-04 13:09:55 -0300

    minimos cambios en estilo de consola de debug

--- src/resources/views/guess-the-number/layout.blade.php
+++ src/resources/views/guess-the-number/layout.blade.php
@@ -6,16 +6,18 @@
         </h2>
     </x-slot>
 
-    <div class="text-center p-3 relative">
+    <div class="text-center p-5 relative">
         <x-event-listener />
         {{ $slot }}
         {{-- A collapsible/expenddible debug console --}}
-        <details class="absolute top-0 right-0 w-3/4">
-            <summary class="cursor-pointer text-slate-600 p-1 text-right"></summary>
+        <details class="absolute top-0 right-0 w-4/5">
+            <summary class="cursor-pointer text-slate-600 text-right"></summary>
             {{-- A Clear console button --}}
-            <x-button class="active:" onclick="document.getElementById('debug-console').textContent = ''"><x-clear-icon/></x-button>
-            <div id="debug-console"
-                class="text-left text-gray-200 font-mono text-xs p-2 bg-slate-900 border border-collapse shadow-lg h-64 max-h-64 overflow-y-scroll">
+            <div class="w-full text-left">
+                <x-button class="absolute top-0 left-0" onclick="document.getElementById('debug-console').textContent = ''"><x-clear-icon /></x-button>
+                <div id="debug-console"
+                    class="absolute top-10 left-0 text-left text-gray-200 font-mono text-xs p-2 bg-slate-900 border shadow-lg h-64 max-h-64 overflow-y-scroll w-full">
+                </div>
             </div>
         </details>
     </div>
