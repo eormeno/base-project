258ac41369046e9bf360275861404e0814056656
DAMO\Emilio <eormeno@gmail.com>
2024-12-11 12:14:30 -0300

    canva test

--- src/resources/views/game-app/webgl.blade.php
+++ src/resources/views/game-app/webgl.blade.php
@@ -1,4 +1,15 @@
+<!--
+for most samples webgl-utils only provides shader compiling/linking and
+canvas resizing because why clutter the examples with code that's the same in every sample.
+See https://webglfundamentals.org/webgl/lessons/webgl-boilerplate.html
+and https://webglfundamentals.org/webgl/lessons/webgl-resizing-the-canvas.html
+for webgl-utils, m3, m4, and webgl-lessons-ui.
+-->
+<script src="https://webglfundamentals.org/webgl/resources/webgl-utils.js"></script>
+
 <x-guess-the-number-layout>
+    <script src="{{ asset('js/webgl-go-renderer.js') }}"></script>
+
     <x-slot name="title">
         {{ __("$currentGame->title") }}
     </x-slot>
@@ -11,21 +22,20 @@
 
 <style>
     @import url("https://webglfundamentals.org/webgl/resources/webgl-tutorials.css");
+
     canvas {
-        width: 50vw;
-        height: 50vh;
+        width: 30vw;
+        height: 30vw;
         display: block;
     }
 </style>
 
 <script  id="vertex-shader-2d" type="notjs">
-
   // an attribute will receive data from a buffer
   attribute vec4 a_position;
 
   // all shaders have a main function
   void main() {
-
     // gl_Position is a special variable a vertex shader
     // is responsible for setting
     gl_Position = a_position;
@@ -33,7 +43,6 @@
 
 </script>
 <script  id="fragment-shader-2d" type="notjs">
-
   // fragment shaders don't have a default precision so we need
   // to pick one. mediump is a good default
   precision mediump float;
@@ -41,14 +50,7 @@
   void main() {
     // gl_FragColor is a special variable a fragment shader
     // is responsible for setting
-    gl_FragColor = vec4(1, 0, 0.5, 1); // return redish-purple
+    gl_FragColor = vec4(0.1, 0.7, 0.5, 1); // return redish-purple
   }
 
-</script><!--
-for most samples webgl-utils only provides shader compiling/linking and
-canvas resizing because why clutter the examples with code that's the same in every sample.
-See https://webglfundamentals.org/webgl/lessons/webgl-boilerplate.html
-and https://webglfundamentals.org/webgl/lessons/webgl-resizing-the-canvas.html
-for webgl-utils, m3, m4, and webgl-lessons-ui.
--->
-<script src="https://webglfundamentals.org/webgl/resources/webgl-utils.js"></script>
+</script>
