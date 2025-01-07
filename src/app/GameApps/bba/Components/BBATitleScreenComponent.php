b0ae587abed4ef96fdfd00caa55d826353ca5173
Emilio Ormeño <eormneno@gmail.com>
2025-01-07 13:32:38 -0300

    refactor BBATitleScreenComponent to extend PersistentComponent and simplify structure; update Root prefab to define states

--- src/app/GameApps/bba/Components/BBATitleScreenComponent.php
+++ src/app/GameApps/bba/Components/BBATitleScreenComponent.php
@@ -2,30 +2,14 @@
 
 namespace App\GameApps\bba\Components;
 
-use App\Contracts\IPersistent;
-use App\Models\Components\StateViewComponent;
+use App\Models\Components\PersistentComponent;
 
-class BBATitleScreenComponent extends StateViewComponent implements IPersistent
+class BBATitleScreenComponent extends PersistentComponent
 {
     protected $table = 'bba_title_screen_components';
 
     public static function config(): array
     {
         return [];
-    }
-
-    public function getTable(): string
-    {
-        return $this->table;
-    }
-
-    public function getFillable(): array
-    {
-        $config = [];
-        // if current class has a config method, then call it
-        if (method_exists($this, 'config')) {
-            $config = $this->config();
-        }
-        return array_merge(array_keys($config), ['id']);
     }
 }
--- src/app/GameApps/bba/prefabs/root.php
+++ src/app/GameApps/bba/prefabs/root.php
@@ -6,58 +6,10 @@ use App\Models\Prefab\Prefab;
 
 class Root extends Prefab
 {
-    public static function structure(): array
-    {
-        return [
-            'components' => [
-                'bba.initial-state' => [],
-                'sprite-renderer' => [
-                    'texture' => 'background.jpeg',
-                    'layer' => 0,
-                ],
-            ],
-            'children' => [
-                'bba.ball' => [],
-                'slot' => []
-            ]
-
-            // 'children' => [
-            //     [
-            //         'name' => 'Child 1',
-            //         'active' => false,
-            //         'components' => [
-            //             'gtn.game-data' => [],
-            //         ],
-            //         'children' => [
-            //             [
-            //                 'name' => 'Grandchild 1.1',
-            //                 'components' => [
-            //                     'gtn.game-data' => [],
-            //                 ],
-            //             ],
-            //         ],
-            //     ],
-            //     [
-            //         'name' => 'Child 2',
-            //         'components' => [
-            //             'gtn.game-data' => [],
-            //         ],
-            //         'children' => [
-            //             [
-            //                 'name' => 'Grandchild 2.1',
-            //                 'components' => [
-            //                     'gtn.game-data' => [],
-            //                 ],
-            //             ],
-            //             [
-            //                 'name' => 'Grandchild 2.2',
-            //                 'components' => [
-            //                     'gtn.game-data' => [],
-            //                 ],
-            //             ],
-            //         ],
-            //     ],
-            // ],
-        ];
-    }
+	public static function states(): array
+	{
+		return [
+			'initial' => ['bba.bouncing' => []],
+		];
+	}
 }
