6c780acad9c79a6ec0e173e5ddb32451a5da5c3c
Emilio Ormeño <eormneno@gmail.com>
2025-01-23 11:40:43 -0300

    refactor test script; add filtering capability for running specific test classes

--- test.ps1
+++ test.ps1
@@ -8,4 +8,30 @@ if ($currentPath -match 'src$' -or $currentPath -match 'src\\$') {
 }
 
 Set-Location src
-php artisan test
+
+$filter = $args[0]
+if ($filter -eq $null) {
+    # if no filter is provided, then run all tests
+    php artisan test
+    return
+}
+
+# A dictionary with the prefix and the test class name
+$testClasses = @{
+    'gtn' = 'GTNPlayGameTest';
+    'bba' = 'BBAPlayGameTest';
+}
+
+# Find the filter in prefix and get the test class name
+$testClass = $testClasses[$filter]
+
+if ($testClass -eq $null) {
+    Write-Host "Invalid filter. Available filters are:" -ForegroundColor Red
+    foreach ($key in $testClasses.Keys) {
+        Write-Host "  $key" -ForegroundColor Yellow -NoNewline
+        Write-Host " - $($testClasses[$key])" -ForegroundColor Green
+    }
+    return
+}
+
+php artisan test --filter=$testClass
