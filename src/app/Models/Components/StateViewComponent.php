13e33f58c6a3eefbe567ca8f26f7a66f2ccea342
Emilio Ormeño <eormneno@gmail.com>
2025-01-23 22:00:06 -0300

    refactor StateViewComponent; remove unused properties and commented-out methods

--- src/app/Models/Components/StateViewComponent.php
+++ src/app/Models/Components/StateViewComponent.php
@@ -7,9 +7,6 @@ use App\Utils\ReflectionUtils;
 
 abstract class StateViewComponent extends Component
 {
-	//protected $fillable = ['id'];
-	//protected $view_name = 'default';
-
 	public function handleStateEvent(array $event): string|null
 	{
 		$eventName = $event['event'];
@@ -51,13 +48,4 @@ abstract class StateViewComponent extends Component
 	{
 		return null;
 	}
-
-	// public function view()
-	// {
-	// 	$data = $this->messages();
-	// 	if (!isset($this->view_name)) {
-	// 		$this->view_name = 'default';
-	// 	}
-	// 	return base64_encode(view($this->view_name, $data));
-	// }
 }
