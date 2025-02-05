7f208197716cf5c478104faa88b66821e45d372a
Emilio Ormeño <eormneno@gmail.com>
2025-02-05 14:06:41 -0300

    update namespace method to include 'services' in pascalization for enhanced component organization

--- src/app/Helpers/ComponentsMigration.php
+++ src/app/Helpers/ComponentsMigration.php
@@ -57,7 +57,7 @@ class ComponentsMigration extends Migration
 		$words = explode('_', $name);
 		// remove the last word (migrations)
 		array_pop($words);
-		$this->pascalize($words, ['common', 'components']);
+		$this->pascalize($words, ['common', 'components', 'services']);
 		// join the words with a '/'
 		$namespace = "GameApps/" . implode('/', $words);
 		$path = app_path($namespace);
