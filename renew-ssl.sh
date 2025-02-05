1227d5981f5d685f62fffa32ef162642093470dc
Emilio Ormeño <eormneno@gmail.com>
2025-02-05 19:38:27 -0300

    add clear command to renew-ssl.sh for improved script readability

--- renew-ssl.sh
+++ renew-ssl.sh
@@ -1,3 +1,4 @@
+clear
 sudo certbot certonly --webroot --webroot-path ~/base-project/src/public -d damogame.com
 sudo cp -r -L /etc/letsencrypt/live/damogame.com/fullchain.pem /etc/letsencrypt/ssl/
 sudo cp -r -L /etc/letsencrypt/live/damogame.com/privkey.pem /etc/letsencrypt/ssl/
