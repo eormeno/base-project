51a55bbde80158ba2703261fdc35851a8be0a399
Emilio Ormeño <eormneno@gmail.com>
2025-02-05 19:43:10 -0300

    update renew-ssl.sh to use absolute path for webroot

--- renew-ssl.sh
+++ renew-ssl.sh
@@ -1,5 +1,4 @@
-clear
-sudo certbot certonly --webroot --webroot-path ~/base-project/src/public -d damogame.com
+sudo certbot certonly --webroot --webroot-path /home/admin/base-project/src/public -d damogame.com
 sudo cp -r -L /etc/letsencrypt/live/damogame.com/fullchain.pem /etc/letsencrypt/ssl/
 sudo cp -r -L /etc/letsencrypt/live/damogame.com/privkey.pem /etc/letsencrypt/ssl/
 sudo chown admin:admin /etc/letsencrypt/ssl/fullchain.pem
