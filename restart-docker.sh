ebadf2cd1bacec5834d7239a7f4ebbe61789f12c
Emilio Ormeño <eormneno@gmail.com>
2025-02-05 19:52:06 -0300

    refactor restart-docker.sh to improve echo command formatting and add container status display

--- restart-docker.sh
+++ restart-docker.sh
@@ -1,27 +1,29 @@
 # Stop containers
 sudo docker stop $(docker ps -a -q) &>/dev/null
-echo -e "\e[32mAll containers have been stopped.\e[0m"
+echo "\e[32mAll containers have been stopped.\e[0m"
 
 # Remove containers
 sudo docker rm $(docker ps -a -q) &>/dev/null
-echo -e "\e[32mAll containers have been removed.\e[0m"
+echo "\e[32mAll containers have been removed.\e[0m"
 
 # Remove images
 sudo docker rmi $(docker images -a -q) &>/dev/null
-echo -e "\e[32mAll images have been removed.\e[0m"
+echo "\e[32mAll images have been removed.\e[0m"
 
 # Remove volumes
 sudo docker volume rm $(docker volume ls -q) &>/dev/null
-echo -e "\e[32mAll volumes have been removed.\e[0m"
+echo "\e[32mAll volumes have been removed.\e[0m"
 
 # Restart Docker
 sudo docker compose down -v
-echo -e "\e[32mDocker has been restarted.\e[0m"
+echo "\e[32mDocker has been restarted.\e[0m"
 
 # Remove all unused containers, networks, images (both dangling and unreferenced), and optionally, volumes.
 sudo docker system prune -a -f
-echo -e "\e[32mAll unused containers, networks, images, and volumes have been removed.\e[0m"
+echo "\e[32mAll unused containers, networks, images, and volumes have been removed.\e[0m"
 
 # Restart Docker
 sudo systemctl restart docker
-echo -e "\e[32mDocker has been restarted.\e[0m"
+echo "\e[32mDocker has been restarted.\e[0m"
+
+sudo docker ps -a --format "{{.Names}}\t\t{{.Status}}\t{{.Ports}}"
