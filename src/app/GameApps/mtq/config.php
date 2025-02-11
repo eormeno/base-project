a09eefb99ec432c8d01284bb59865f2de215c62d
Emilio Ormeño <eormneno@gmail.com>
2025-02-11 15:08:02 -0300

    fix(config.php): improve game description for clarity and detail

--- src/app/GameApps/mtq/config.php
+++ src/app/GameApps/mtq/config.php
@@ -4,7 +4,7 @@ return [
     'image' => 'mythic-treasure-quest.jpeg',
     'active' => true,
     'name' => 'Buscador de Tesoros',
-    'description' => 'Un juego donde exploras templos antiguos y encuentras tesoros y posiones usando las mecánicas de buscaminas. Pero ten cuidado! También hay trampas, monstruos y maldiciones.',
+    'description' => 'Un juego en donde exploras templos, palacios y criptas antiguas y encuentras tesoros y posiones usando las mecánicas del clásico juego buscaminas. ¡Pero ten cuidado! También hay trampas, monstruos y maldiciones.',
 	'client' => 'webgl',
     'width' => 600,
     'height' => 700,
