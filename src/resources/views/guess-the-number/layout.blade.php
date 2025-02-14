1cbd7977a964900cb3e359de06eaa7936fffa077
Emilio Ormeño <eormneno@gmail.com>
2025-02-14 10:32:06 -0300

    refactor(guess-the-number layout): comment out unused event listener and debug console elements for improved clarity

--- src/resources/views/guess-the-number/layout.blade.php
+++ src/resources/views/guess-the-number/layout.blade.php
@@ -7,19 +7,19 @@
     </x-slot>
 
     <div class="text-center p-3 relative">
-        <x-event-listener />
+        {{-- <x-event-listener /> --}}
         {{ $slot }}
         {{-- A collapsible/expenddible debug console --}}
-        <details class="absolute top-0 right-0 w-4/5">
-            <summary class="cursor-pointer text-slate-600 text-right"></summary>
+        {{-- <details class="absolute top-0 right-0 w-4/5"> --}}
+            {{-- <summary class="cursor-pointer text-slate-600 text-right"></summary> --}}
             {{-- A Clear console button --}}
-            <div class="w-full text-left">
-                <x-button class="absolute top-0 left-0" onclick="document.getElementById('debug-console').textContent = ''"><x-clear-icon /></x-button>
-                <div id="debug-console"
-                    class="absolute top-10 left-0 text-left text-gray-200 font-mono text-xs p-2 bg-slate-900 border shadow-lg h-64 max-h-64 overflow-y-scroll w-full">
-                </div>
-            </div>
-        </details>
-    </div>
+            {{-- <div class="w-full text-left"> --}}
+                {{-- <x-button class="absolute top-0 left-0" onclick="document.getElementById('debug-console').textContent = ''"><x-clear-icon /></x-button> --}}
+                {{-- <div id="debug-console" --}}
+                    {{-- class="absolute top-10 left-0 text-left text-gray-200 font-mono text-xs p-2 bg-slate-900 border shadow-lg h-64 max-h-64 overflow-y-scroll w-full"> --}}
+                {{-- </div> --}}
+            {{-- </div> --}}
+        {{-- </details> --}}
+    {{-- </div> --}}
 
 </x-app-layout>
